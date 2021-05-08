3a7b3f70a4e5c4a3d378225c83e87403f7882b89
Alexandre Dionisio <aadionisio@gmail.com>
2021-05-08 12:08:04 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import ImgDsDark from 'asssets/img/ds-dark.svg';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
@@ -7,7 +8,10 @@ const NavBar: React.FC = () => {
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <img src={ImgDsDark} alt="DevSuperior" width="120" />
+          <Link to="/">
+            <img src={ImgDsDark} alt="DevSuperior" width="120" />
+          </Link>
+
         </nav>
       </div>
     </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,25 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar"
+import React from "react"
+import { Link } from "react-router-dom";
+
 const Home: React.FC = () => {
   return (
-    <div>
-      <h3>Página Home</h3>
-    </div>
-
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr />
+          <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+          <Link className="btn btn-primary btn-large" to="/dashboard">
+            Acessar o Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
