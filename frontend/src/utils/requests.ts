5a9acd806f00ef115d78063da7e2f266472b3694
Alexandre Dionisio <aadionisio@gmail.com>
2021-05-08 16:01:26 -0300

    Env Var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+// se a variavel de ambiente nao tiver preenchida ele ignora e a base
+// assume o localhost como url padrao
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
